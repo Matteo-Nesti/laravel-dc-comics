d58d09a0efd386b8db59643c7df9b8e8ac78e94c
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-08-25 15:56:47 +0200

    rename home rename controller and create model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,17 +1,18 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Comic extends Controller
+class HomeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return view('home');
     }
 
     /**
