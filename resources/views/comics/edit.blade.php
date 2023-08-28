7a392e95ca2c2fcb493b403557f0f35ef9546327
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-08-28 16:49:14 +0200

    add edit form

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -10,54 +10,57 @@
 
             <div class="mb-3 col-6">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="title" name="title" value="">
+                <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
             </div>
 
             <div class="mb-3 col-6">
                 <label for="thumb" class="form-label">Copertina</label>
-                <input type="text" class="form-control" id="thumb" name="thumb" placeholder="URL immagine">
+                <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}">
             </div>
 
             <div class="mb-3 col-3">
                 <label for="price" class="form-label">prezzo</label>
-                <input type="text" class="form-control" id="price" name="price" placeholder="Prezzo">
+                <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
             </div>
 
             <div class="mb-3 col-3">
                 <label for="series" class="form-label">serie</label>
-                <input type="text" class="form-control" id="series" name="series" placeholder="Serie">
+                <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}">
             </div>
 
             <div class="mb-3 col-3">
                 <label for="type" class="form-label">Tipo di scrittura</label>
-                <input type="text" class="form-control" id="type" name="type" placeholder="Tipo di scrittura">
+                <input type="text" class="form-control" id="type" name="type" value="{{ $comic->type }}">
             </div>
 
             <div class="mb-3 col-3">
                 <label for="sale_date" class="form-label">Data di Rilascio</label>
-                <input type="text" class="form-control" id="sale_date" name="sale_date" placeholder="Data di rilascio">
+                <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
             </div>
 
             <div class="mb-3 col-6">
                 <label for="artists" class="form-label">Artisti</label>
-                <textarea class="form-control" id="artists" rows="3" name="artists">
-            </textarea>
+                <textarea class="form-control" id="artists" rows="5" name="artists">
+                    {{ $comic->artists }}
+                </textarea>
             </div>
 
             <div class="mb-3 col-6">
                 <label for="writers" class="form-label">Scrittori</label>
-                <textarea class="form-control" id="writers" rows="3" name="writers">
-            </textarea>
+                <textarea class="form-control" id="writers" rows="5" name="writers">
+                    {{ $comic->writers }}
+                </textarea>
             </div>
 
             <div class="mb-3 col-12">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control" id="description" rows="3" name="description">
-            </textarea>
+                <textarea class="form-control" id="description" rows="5" name="description">
+                    {{ $comic->description }}
+                </textarea>
             </div>
         </div>
         <div class="d-flex justify-content-around">
-            <button type="submit" class="btn btn-success">Aggiungi</button>
+            <button type="submit" class="btn btn-success">Modifica</button>
             <button type="reset" class="btn btn-danger ">Svuota</button>
             <a href="{{ route('comics.index') }}" class="btn btn-secondary">torna indietro</a>
         </div>
